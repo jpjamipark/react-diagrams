0a36d983b07f3d4d070f1a45a4c3c6d451def010
Noah Trenaman <noahtren@gmail.com>
2020-01-16 23:38:07 -0500

    add standlone project

---
+++ project/README.md
@@ -0,0 +1,10 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+This is a simple, standalone demo of the `react-diagrams` library and a starting point for creating
+a visual authoriing environment for neural networks.
+
+## Get started
+
+`yarn install`
+then
+`npm start` or `yarn run start`
---
+++ project/src/App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import createEngine, { 
+    DefaultLinkModel,
+    DiagramModel,
+    DefaultNodeModel,
+} from '@projectstorm/react-diagrams';
+
+import {
+    CanvasWidget
+} from '@projectstorm/react-canvas-core';
+
+import { DemoCanvasWidget } from './helpers/DemoCanvasWidget';
+
+
+function get_starter_model() {
+    // node 1
+    const node1 = new DefaultNodeModel({
+        name: 'Node 1',
+        color: 'rgb(0,192,255)',
+    });
+    node1.setPosition(100, 100);
+    let port1 = node1.addOutPort('Out');
+
+    // node 2
+    const node2 = new DefaultNodeModel({
+        name: 'Node 2',
+        color: 'rgb(0,192,255)',
+    });
+    node2.setPosition(500, 100);
+    let port2 = node2.addInPort('In');
+
+    // link them and add a label to the link
+    const link = port1.link<DefaultLinkModel>(port2);
+    link.addLabel('Welcome to the AI Blockchain');
+
+    const model = new DiagramModel();
+    model.addAll(node1, node2, link);
+    return model;
+}
+
+
+function App() {  
+  let starter_model = get_starter_model();
+  const engine = createEngine();
+  engine.setModel(starter_model);
+  return (
+    <DemoCanvasWidget>
+        <CanvasWidget engine={engine}/>
+    </DemoCanvasWidget>
+  );
+}
+
+export default App;
